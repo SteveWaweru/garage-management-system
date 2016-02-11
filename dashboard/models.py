c0b8b9cdd30d23ede9273ddd81572f6d4f564101
steve <steviewez@gmail.com>
2016-02-11 15:59:28 +0300

    added ui theme

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,4 +2,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField(blank=True, null=True, unique=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    vehicle_make = models.CharField(max_length=20, blank=True, null=True)
+    vehicle_type = models.CharField(max_length=20, blank=True, null=True)
+    vehicle_registration_number = models.CharField(max_length=20, blank=True, null=True)
+    vehicle_chasis_number = models.CharField(max_length=20, blank=True, null=True)
+    installation_date = models.DateTimeField()
+    expiry_date = models.DateTimeField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Payment(models.Model):
+    customer = models.ForeignKey(Customer)
+    mode_of_payment = models.CharField(max_length=20, choices=(
+        ('CASH', 'Cash'),
+        ('BANK DEPOSIT', 'Bank Deposit'),
+        ('CHEQUE', 'Cheque'),
+        ('RTGS', 'RTGS'),
+    ))
+    name_payer = models.CharField(max_length=20, blank=True, null=True)
+    account_name = models.CharField(max_length=20, blank=True, null=True)
+    bank = models.CharField(max_length=20, blank=True, null=True)
+    amount = models.IntegerField()
+    amount_due = models.IntegerField(blank=True, null=True)
+    payment_status = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.customer.name + "-->" + str(self.amount)
--- paymentsys/settings.py
+++ paymentsys/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -120,4 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
