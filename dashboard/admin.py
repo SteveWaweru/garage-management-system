f5a1bda923987a86d8d0606a00c0fbfd09345df6
stevewaweru <steviewez@gmail.com>
2016-06-24 02:23:40 +0300

    add Payment

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Customer, Payment, Vehicle,Invoice
+from .models import Customer, Payment, Vehicle, Invoice, Account
 # Register your models here.
 
 
@@ -7,3 +7,4 @@ admin.site.register(Customer)
 admin.site.register(Payment)
 admin.site.register(Vehicle)
 admin.site.register(Invoice)
+admin.site.register(Account)
